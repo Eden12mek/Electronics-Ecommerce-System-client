4b351be03c34ff3846d9ec726523dd1d0c41a6ab
eden <eden12mekonnen@gmail.com>
2025-05-23 15:53:17 +0300

    footer modification

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className=''style={{ backgroundColor: '#FFFCFA' }}>
     <div className='container mx-auto p-4'>
-        <p>Electronics Ecommerce Shopping Center @ 2024</p>
+        <p>KHM Electronics Ecommerce Shopping Center @ 2025</p>
         </div>
     </footer>
   )
